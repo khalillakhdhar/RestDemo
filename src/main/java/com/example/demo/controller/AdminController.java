e70fc8795fafdea7a3612807b681ff85e2841dce
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 15:55:40 +0100

    Update AdminController.java

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
 
-public class AdminController {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.repositories.AdminRepository;
+import com.example.demo.entities.Admin;
+@RestController
+public class AdminController {
+@Autowired
+AdminRepository adminRepository;
+@GetMapping("admin")
+public List<Admin> getAdmins()
+{
+return (List<Admin>) adminRepository.findAll();	
+
+}
+@PostMapping("admin")
+public Admin addAdmin(@Valid @RequestBody Admin admin)
+{
+	return adminRepository.save(admin);
+}
 }
